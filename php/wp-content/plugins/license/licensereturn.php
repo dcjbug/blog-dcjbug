23e6e08bddda4c26d14ad8bd36f49d0f79da1b80
Isaac Christoffersen <ichristoffersen@vizuri.com>
2011-10-15 13:26:15 -0400

    Added CC license plugin

---
+++ php/wp-content/plugins/license/licensereturn.php
@@ -0,0 +1,8 @@
+<html>
+<body>
+<script>
+parent.setLicense({url:'<?php echo $_GET["url"]?>',name:'<?php echo $_GET["name"]?>',button:'<?php echo $_GET["button"]?>',deed:'<?php echo $_GET["deed"]?>',});
+parent.tb_remove();
+</script>
+</body>
+</html>
